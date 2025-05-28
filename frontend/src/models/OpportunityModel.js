d81af732e02d8daab46ad49b7d9e3f8475463e76
Gabriel_Leal <glealleone@gmail.com>
2025-05-28 17:05:38 -0300

    removendo pasta desnecessaria e refactor do frontend

--- frontend/src/models/OpportunityModel.js
+++
@@ -1,8 +0,0 @@
-// src/models/OpportunityModel.js
-import { fetchOpportunities } from '../services/api';
-
-// Garanta que está exportando corretamente
-export const getOpportunities = async () => {
-  const response = await fetchOpportunities();
-  return response.data; 
-};
